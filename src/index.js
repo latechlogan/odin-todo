b245db89c106449a7e8e646c58ac0d04816dc34a
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-03 16:21:55 -0500

    Remove calls to test module imports from index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ import storageManager from "./modules/storageManager";
 import timelineManager from "./modules/timelineManager";
 import todoManager from "./modules/todoManager";
 import uiController from "./modules/uiController";
-
-eventBus.logger();
-storageManager.logger();
-timelineManager.logger();
-todoManager.logger();
-uiController.logger();
