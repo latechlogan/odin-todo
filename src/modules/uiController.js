5b4a8e05bb67a0a8079a3a797ca1a84910e8d95a
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-04 11:01:15 -0500

    Add task form submission simulation for testing

--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -1,9 +1,18 @@
-const uiController = (function () {
-  const lockedIn = "uiController.js is locked in!";
+import eventBus from "./eventBus";
 
-  return {
-    logger: () => console.log(lockedIn),
+const uiController = (function () {
+  const testSubmission = {
+    title: "Pickup Dry Cleaning",
+    dueDateString: "2025/09/05",
+    description: "Pickup the dry cleaning at Weil Cleaning on North 7th",
+    importance: 1,
   };
+
+  const getSubmission = function () {
+    eventBus.emit("formSubmitted", testSubmission);
+  };
+
+  return { getSubmission };
 })();
 
 export default uiController;
