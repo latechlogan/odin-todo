c08e2f77d046c82d894edb6cb55c4cf817108d6f
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-04 12:05:03 -0500

    Add tasks array & supporting func to todoManager

--- src/modules/todoManager.js
+++ src/modules/todoManager.js
@@ -2,6 +2,8 @@ import eventBus from "./eventBus.js";
 import Task from "../models/Task.js";
 
 const todoManager = (function () {
+  const tasks = [];
+
   const createTask = function (object) {
     const task = new Task(
       object.title,
@@ -9,10 +11,22 @@ const todoManager = (function () {
       object.description,
       object.importance
     );
-    eventBus.emit("taskAdded", task);
     console.log(task);
+    return task;
   };
-  eventBus.on("formSubmitted", createTask);
+
+  const addTask = function (task) {
+    tasks.push(task);
+    console.log(tasks);
+    eventBus.emit("taskAdded", tasks);
+  };
+
+  const handleFormSubmit = function (object) {
+    const task = createTask(object);
+    addTask(task);
+  };
+
+  eventBus.on("formSubmitted", handleFormSubmit);
 })();
 
 export default todoManager;
