553d0a74dd5e9fd19fdd88bd501b74bc0b2b788a
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-04 21:59:26 -0500

    Add app init function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import "./styles.css";
+
+import Task from "./models/Task";
+import TaskGroup from "./models/TaskGroup";
+
 import eventBus from "./modules/eventBus";
 import storageManager from "./modules/storageManager";
 import timelineManager from "./modules/timelineManager";
 import todoManager from "./modules/todoManager";
 import uiController from "./modules/uiController";
+
+// attached to window for testing in browser console
+window.TaskGroup = TaskGroup;
+window.uiController = uiController;
+
+const initApp = (function () {
+  console.log("Initializing Todo Timeline App...");
+  eventBus.emit("appStart");
+})();
