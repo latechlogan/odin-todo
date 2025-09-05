4c8117c2a234acda247582863c36215758d5ea80
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-04 21:28:36 -0500

    Add task grouping functionality to timelineManager

--- src/modules/timelineManager.js
+++ src/modules/timelineManager.js
@@ -1,9 +1,24 @@
-const timelineManager = (function () {
-  const lockedIn = "timelineManager.js is locked in!";
+import eventBus from "./eventBus";
 
-  return {
-    logger: () => console.log(lockedIn),
+const timelineManager = (function () {
+  const groupTasksByDate = function (tasks) {
+    const groups = new Map();
+
+    tasks.forEach((task) => {
+      const dateKey = task.dueDate.toDateString();
+
+      if (!groups.has(dateKey)) {
+        groups.set(dateKey, new TaskGroup(task.dueDate));
+      }
+
+      groups.get(dateKey).tasks.push(task);
+    });
+
+    console.log("Grouped tasks array: ", Array.from(groups.values()));
+    return Array.from(groups.values());
   };
+
+  eventBus.on("taskAdded", groupTasksByDate);
 })();
 
 export default timelineManager;
