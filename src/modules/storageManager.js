0af780d1dfc56a1106a1a39543e47ee931466c94
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-06 21:38:32 -0500

    Add appStart event handling to storageManager

--- src/modules/storageManager.js
+++ src/modules/storageManager.js
@@ -14,13 +14,15 @@ const storageManager = (() => {
   };
 
   const load = function () {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    const loadedTasks = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    eventBus.emit("tasksLoaded", loadedTasks);
   };
 
   const clear = function () {
     localStorage.removeItem(STORAGE_KEY);
   };
 
+  eventBus.on("appStart", load);
   eventBus.on("tasksChanged", save);
 })();
 
