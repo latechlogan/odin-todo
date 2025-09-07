46a6fca4acea66c92b0201c2fe0af0b490fa5c44
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-06 21:56:48 -0500

    Refactor uiController test task submit function

--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -1,14 +1,14 @@
 import eventBus from "./eventBus";
 
 const uiController = (function () {
-  const testSubmission = {
-    title: "Pickup Dry Cleaning",
-    dueDateString: "2025/09/05",
-    description: "Pickup the dry cleaning at Weil Cleaning on North 7th",
-    importance: 1,
-  };
-
-  const getSubmission = function () {
+  const getSubmission = function (options = {}) {
+    const defaults = {
+      title: "Pickup Dry Cleaning",
+      dueDateString: new Date(),
+      description: "Pickup the dry cleaning at Weil Cleaning on North 7th",
+      importance: 1,
+    };
+    const testSubmission = { ...defaults, ...options };
     eventBus.emit("formSubmitted", testSubmission);
   };
 
