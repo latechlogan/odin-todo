3dd0edcfc8a8ac4e91fc9231559d02d6c53eb6f5
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-06 22:38:23 -0500

    Remove timelineManager module since not needed

--- src/modules/timelineManager.js
+++
@@ -1,9 +0,0 @@
-const timelineManager = (function () {
-  const lockedIn = "timelineManager.js is locked in!";
-
-  return {
-    logger: () => console.log(lockedIn),
-  };
-})();
-
-export default timelineManager;
