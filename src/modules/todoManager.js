198df00b5532e67f320e9c5457bc5a22f28f26b7
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-07 15:32:55 -0500

    Add TaskGroup creation and handling to todoManager

--- src/modules/todoManager.js
+++ src/modules/todoManager.js
@@ -1,15 +1,18 @@
 import eventBus from "./eventBus.js";
 import Task from "../models/Task.js";
+import TaskGroup from "../models/TaskGroup.js";
 
 const todoManager = (function () {
   const tasks = [];
+  const groups = [];
 
   const createTask = function (object) {
     const task = new Task(
       object.title,
       object.dueDateString,
       object.description,
-      object.importance
+      object.importance,
+      object.groupId
     );
     addTask(task);
   };
@@ -23,6 +26,22 @@ const todoManager = (function () {
     loadedTasks.forEach((task) => {
       tasks.push(task);
       eventBus.emit("tasksChanged", tasks);
+    });
+  };
+
+  const createTaskGroup = function (name) {
+    const group = new TaskGroup(name);
+    addTaskGroup(group);
+  };
+
+  const addTaskGroup = function (group) {
+    groups.push(group);
+    eventBus.emit("groupsChanged", groups);
+  };
+
+  const moveTasksToGroups = function () {
+    groups.forEach((group) => {
+      TaskGroup.getTasksFromArray(tasks);
     });
   };
 
