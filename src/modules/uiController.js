ee79b32d4c20123b325ed4d2cd379b4ff45f8036
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-07 00:24:49 -0500

    Add logic to uiController for displaying tasks

--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -12,6 +12,31 @@ const uiController = (function () {
     eventBus.emit("formSubmitted", testSubmission);
   };
 
+  const displayTasks = function (tasks) {
+    document.querySelector(".tasks-container").innerHTML = "";
+    tasks.forEach((task) => {
+      const taskDiv = document.createElement("div");
+      taskDiv.classList.add("task-item");
+      taskDiv.dataset.id = task.id;
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = task.completed;
+
+      const title = document.createElement("span");
+      title.textContent = task.title;
+
+      const editBtn = document.createElement("button");
+      editBtn.textContent = "Edit";
+
+      taskDiv.append(checkbox, title, editBtn);
+
+      document.querySelector(".tasks-container").appendChild(taskDiv);
+    });
+  };
+
+  eventBus.on("tasksChanged", displayTasks);
+
   return { getSubmission };
 })();
 
