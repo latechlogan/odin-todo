fbb0604d0d5ccee6e8dffef605f424cb939218e3
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-06 22:45:14 -0500

    Remove timelineModule import from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./styles.css";
 import eventBus from "./modules/eventBus";
 import storageManager from "./modules/storageManager";
-import timelineManager from "./modules/timelineManager";
 import todoManager from "./modules/todoManager";
 import uiController from "./modules/uiController";
 
