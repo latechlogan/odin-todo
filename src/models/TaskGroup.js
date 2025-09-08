2abd882927b38cf282539b76b554e8f7d9b2cc8e
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-08 10:56:49 -0500

    Add static func to "rehydrate" loaded TaskGroups

--- src/models/TaskGroup.js
+++ src/models/TaskGroup.js
@@ -5,6 +5,14 @@ class TaskGroup {
     this.tasks = [];
   }
 
+  static fromObject(obj) {
+    const group = new TaskGroup(obj.groupName, obj.id);
+
+    group.tasks = obj.tasks;
+
+    return group;
+  }
+
   getTasksFromArray(array) {
     const tasks = array.filter((task) => task.groupId === this.id);
     return tasks;
